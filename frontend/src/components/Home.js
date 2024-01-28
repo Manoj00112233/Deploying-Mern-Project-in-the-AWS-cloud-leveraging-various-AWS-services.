c52da9ac66b431fe6ab1e5187d1af063280978f4
Manoj <vermamanoj000.mv@gmail.com>
2024-01-28 19:43:30 +0530

    first commit

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Home() {
+  const [message, setMessage] = useState("");
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://a2193af1fe5dc40bc847753b64e825aa-4318894.us-west-2.elb.amazonaws.com/")
+      .then((response) => {
+        setMessage(response.data.msg);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://a7c3c45abf70b44f5b104166732173b7-1931260900.us-west-2.elb.amazonaws.com/fetchUser")
+      .then((response) => {
+        setProfile(response.data);
+        
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  },[]);
+
+  
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <div>
+        <h2>Profile</h2>
+        {
+        profile.map((user) => {
+            console.log('user', user)
+          return (
+            <div>
+              <h3>Name: {user.name}</h3>
+              <h3>Age: {user.age}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
